a938fc456045aa534e07bb5635c853a45ced578a
Wellerson <wllrsn.c@gmail.com>
2023-05-15 17:40:55 -0300

    feat: mongoose database

---
+++ database.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export default function connectDatabase() {
+  return new Promise((resolve, reject) => {
+    mongoose.Promise = global.Promise;
+    mongoose.connection
+      .on("error", (error) => reject(error))
+      .on("close", () => console.log("Database connection closed."))
+      .once("open", () => resolve(mongoose.connections[0]));
+
+    mongoose.connect(
+      "mongodb://localhost:27017/node-kafka",
+    );
+  });
+}
