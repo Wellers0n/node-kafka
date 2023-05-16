be517479135206a7909701011f7f593785584fd5
Wellerson <wllrsn.c@gmail.com>
2023-05-16 16:18:52 -0300

    feat: location model

---
+++ src/models/location.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const locations = new Schema({
+  ip: {
+    type: String,
+    required: "IP is requerid",
+  },
+  timestamp: {
+    type: Number,
+    required: "timestamp is requerid",
+  },
+  latitude: {
+    type: Number,
+    required: "latitude is requerid",
+  },
+  longitude: {
+    type: Number,
+    required: "longitude is requerid",
+  },
+  country: {
+    type: String,
+    required: "country is requerid",
+  },
+  region: {
+    type: String,
+    required: "region is requerid",
+  },
+  city: {
+    type: String,
+    required: "city is requerid",
+  }
+});
+
+export default mongoose.model("locations", locations);
